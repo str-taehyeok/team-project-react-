54b14b2a807757e0634e55e7e019e0a56dcec7a5
dev-jane99 <ruejaeeun99@gmail.com>
2024-11-21 19:30:46 +0900

    재은 common.js 완료

--- src/global/common.js
+++ src/global/common.js
@@ -19,18 +19,122 @@ export const flexCenterRowSpaceBetween = css`
     justify-content: space-between;
     align-items: center;
 `
-
+// bold font
 export const h1Bold = css`
     font-size: 40px;
     line-height: 54px;
     font-weight: 700;
 `
 
+export const h2Bold = css`
+    font-size: 34px;
+    line-height: 42px;
+    font-weight: 700;
+`
+export const h3Bold = css`
+    font-size: 30px;
+    line-height: 54px;
+    font-weight: 700;
+`
+export const h4Bold = css`
+    font-size: 28px;
+    line-height: 36px;
+    font-weight: 700;
+`
+export const h5Bold = css`
+    font-size: 24px;
+    line-height: 30px;
+    font-weight: 700;
+`
+export const h6Bold = css`
+    font-size: 20px;
+    line-height: 28px;
+    font-weight: 700;
+`
+export const h7Bold = css`
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 700;
+`
+export const h8Bold = css`
+    font-size: 14px;
+    line-height: 22px;
+    font-weight: 700;
+`
+export const h9Bold = css`
+    font-size: 12px;
+    line-height: 20px;
+    font-weight: 700;
+`
+export const h10Bold = css`
+    font-size: 11px;
+    line-height: 19px;
+    font-weight: 700;
+`
+export const h11Bold = css`
+    font-size: 10px;
+    line-height: 18px;
+    font-weight: 700;
+`
+
+
+// medium font
+export const h1Medium = css`
+    font-size: 40px;
+    line-height: 54px;
+    font-weight: 400;
+`
+export const h2Medium = css`
+    font-size: 34px;
+    line-height: 42px;
+    font-weight: 400;
+`
+export const h3Medium = css`
+    font-size: 30px;
+    line-height: 38px;
+    font-weight: 400;
+`
+export const h4Medium = css`
+    font-size: 28px;
+    line-height: 36px;
+    font-weight: 400;
+`
+export const h5Medium = css`
+    font-size: 24px;
+    line-height: 30px;
+    font-weight: 400;
+`
+export const h6Medium = css`
+    font-size: 20px;
+    line-height: 28px;
+    font-weight: 400;
+`
 export const h7Medium = css`
     font-size: 16px;
     line-height: 24px;
     font-weight: 400;
 `
+export const h8Medium = css`
+    font-size: 14px;
+    line-height: 22px;
+    font-weight: 400;
+`
+export const h9Medium = css`
+    font-size: 12px;
+    line-height: 20px;
+    font-weight: 400;
+`
+export const h10Medium = css`
+    font-size: 11px;
+    line-height: 19px;
+    font-weight: 400;
+`
+export const h11Medium = css`
+    font-size: 16px;
+    line-height: 18px;
+    font-weight: 400;
+`
+
 
 
 
