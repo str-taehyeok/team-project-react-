625795d6295c6c931661349fa3cd481fdbc845e8
dev-minchul <minchul766@gmail.com>
2024-11-22 17:47:38 +0900

    로그인

--- src/pages/login/SellerLogin.jsx
+++ src/pages/login/SellerLogin.jsx
@@ -1,67 +1,86 @@
 import React from 'react';
+import S from "./style";
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const SellerLogin = () => {
 
-  const { 
-        register, handleSubmit, formState: {isSubmitting, errors}
-    } = useForm({mode:"onChange"});
+  const {
+    register, handleSubmit, formState: { isSubmitting, errors }
+  } = useForm({ mode: "onChange" });
 
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
 
   return (
     <form onSubmit={handleSubmit(async (data) => {
-       await fetch("http://localhost:10000/login", {
-        method : 'POST',
-        headers : {
-            'Content-Type' : 'application/json',
+      await fetch("http://localhost:10000/login", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-        body : JSON.stringify({
-            // 로그인 데이터
+        body: JSON.stringify({
+          // 로그인 데이터
         })
-       })
+      })
     })}>
-
-      <label>
-        <p>이메일</p>
-        <input type="text" id='email' placeholder='아이디를 입력하세요.' 
-          {...register("email", {
-            required : true,
-            pattern : {
-              value : emailRegex,
-            }
-          })}
-        />
-        {errors && errors?.email?.type === "required" && (
-          <p>이메일을 입력하세요</p>
-        )}
-        {errors && errors?.email?.type === "pattern" && (
-          <p>이메일 양식에 맞게 입력해주세요.</p>
-        )}
-      </label>
+      <S.InputBox>
+        <label>
+          <S.Input type="text" id='email' placeholder='판매자 아이디(이메일)를 입력해주세요'
+            {...register("email", {
+              required: true,
+              pattern: {
+                value: emailRegex,
+              }
+            })}
+          />
+          {errors && errors?.email?.type === "required" && (
+            <S.P>이메일을 입력하세요</S.P>
+          )}
+          {errors && errors?.email?.type === "pattern" && (
+            <S.P>이메일 양식에 맞게 입력해주세요.</S.P>
+          )}
+        </label>
+      </S.InputBox>
 
       {/* 비밀번호 로직 만들기 */}
       <label>
-        <p>비밀번호</p>
-        <input type="password" id='password' placeholder='비밀번호를 입력하세요.' 
+        <S.Input type="password" id='password' placeholder='비밀번호를 입력하세요.'
           autoComplete='off'
           {...register("password", {
-            required : true,
-            pattern : {
-              value : passwordRegex,
+            required: true,
+            pattern: {
+              value: passwordRegex,
             }
           })}
         />
         {errors && errors?.password?.type === "required" && (
-          <p>비밀번호를 입력하세요</p>
+          <S.P>비밀번호를 입력하세요</S.P>
         )}
         {errors && errors?.password?.type === "pattern" && (
-          <p>소문자, 숫자, 특수문자를 각 하나 포함한 8자리 이상이여야 합니다.</p>
+          <S.P>소문자, 숫자, 특수문자를 각 하나 포함한 8자리 이상이여야 합니다.</S.P>
         )}
       </label>
 
-      <button disabled={isSubmitting}>로그인</button>
+      <S.LoginButton disabled={isSubmitting}>로그인</S.LoginButton>
+      <S.Box4>
+        <S.Box3>
+          <S.SaveBox>
+            <S.SaveEmail type='checkbox'></S.SaveEmail>
+            <S.IdSave>아이디 저장</S.IdSave>
+          </S.SaveBox>
+          <Link to={"/findAccount"}>
+            <S.Forgot>아이디 혹은 비밀번호를 잊어버리셨나요?</S.Forgot>
+          </Link>
+        </S.Box3>
+      </S.Box4>
+      <S.TextBox>
+        <S.Text>계정이 없으신가요?</S.Text>
+        <S.TextDivider>|</S.TextDivider>
+        <Link to={"/join"}>
+          <S.JoinButton name="action" value="join">회원가입</S.JoinButton>
+        </Link>
+      </S.TextBox>
     </form>
   );
 };
--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,91 +1,223 @@
 import styled from "styled-components";
-import {
-  flexRowCenter
-} from '../../global/common'
-const S = {}
+import { flexRowCenter } from '../../global/common';
 
+const S = {};
+
+// Root container
 S.root = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-`
+`;
 
-// logo CSS
-  S.LoginMain = styled.div`
-    align-items: center; 
-    justify-content: center;
-    width: 464px;
-    padding-bottom: 40px;
-  `
+// Login Main Section
+S.LoginMain = styled.div`
+  align-items: center; 
+  justify-content: center;
+  width: 464px;
+  padding-bottom: 40px;
+`;
 
+// Logo Box
 S.LogoBox = styled.div`
   display: flex;
   align-items: center; 
   justify-content: center;
   width: 464px;
   padding-bottom: 40px;
-  
-  & img{
+
+  & img {
     width: 236px;
     height: 60px;
     box-sizing: border-box;
   }
-  `
-  //Login CSS
-  S.MemberBtn = styled.div`
-    width: 220px;
-    height: 44px;
-    border-radius: 20px;
-    border: solid 1px #828282;
-    margin-right: 24px;
-    font-size: 16px;
-    font-weight: bold;
-    color: #828282;
-    background-color:#ffffff;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    &:focus{
-      background-color:#828282;
-      color: #ffffff;
-    }
-  `
-  S.SellerBtn = styled.div`
-    width: 220px;
-    height: 44px;
-    border-radius: 20px;
-    border: solid 1px #828282;
-    font-size: 16px;
-    font-weight: bold;
-    color: #828282;
-    background-color:#ffffff;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    &:focus{
-      background-color:#828282;
-      color: #ffffff;
-    }
-  `
-  S.Tap = styled.div`
+`;
+
+// Buttons
+S.MemberBtn = styled.div`
+  width: 220px;
+  height: 44px;
+  border-radius: 20px;
+  border: solid 1px #828282;
+  margin-right: 24px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #828282;
+  background-color: #ffffff;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:focus {
+    background-color: #828282;
+    color: #ffffff;
+  }
+`;
+
+S.SellerBtn = styled.div`
+  width: 220px;
+  height: 44px;
+  border-radius: 20px;
+  border: solid 1px #828282;
+  font-size: 16px;
+  font-weight: bold;
+  color: #828282;
+  background-color: #ffffff;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:focus {
+    background-color: #828282;
+    color: #ffffff;
+  }
+`;
+
+// Input & Tap
+S.Tap = styled.div`
   margin-bottom: 20px;
   display: flex;
   flex-direction: row;
+`;
 
-  `
-   S.Input = styled.input`
+S.InputBox = styled.div`
+  margin-bottom: 15px;
+`;
+
+S.Input = styled.input`
+  background-color: #ffffff;
+  width: 464px;
+  height: 60px;
+  border: 0.5px solid #828282;
+  font-size: 14px;
+  color: #828282;
+  border-radius: 20px;
+  box-sizing: border-box;
+  padding: 19px 26px;
+  outline: none;
+`;
+
+// Buttons
+S.LoginButton = styled.button`
+  width: 464px;
+  height: 55px;
+  background-color: #FFC303;
+  font-size: 16px;
+  font-weight: bold;
+  border-style: none;
+  border-radius: 20px;
+  box-sizing: border-box;
+  cursor: pointer;
+  margin-top: 25px;
+`;
+
+// Texts
+S.P = styled.p`
+  color: red;
+`;
+
+S.IdSave = styled.p`
+  font-size: 14px;
+  color: #131313;
+  margin: 0;
+`;
+
+// Checkboxes
+S.SaveBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+S.SaveEmail = styled.input`
+  appearance: none;
+  display: block;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  margin-right: 10px;
+  position: relative;
+  background-color: #D9D9D9;
+  box-sizing: border-box;
+  transition: background-color 0.3s;
+  cursor: pointer;
+
+  &:checked {
+    background-color: yellowgreen;
+  }
+
+  &:checked::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
     background-color: #ffffff;
-    width: 464px;
-    height: 60px;
-    border: 0.5px solid #828282;
-    font-size: 14px;
-    color: #828282;
-    border-radius: 20px;
-    box-sizing: border-box;
-    padding: 19px 26px;
-    outline: none;
-   `
+  }
+`;
+
+// Other Boxes
+S.Box4 = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 464px;
+  padding-top: 20px;
+`;
+
+S.Box3 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center; 
+  width: 427px; 
+`;
+
+// Forgot Password
+S.Forgot = styled.button`
+  font-size: 14px;
+  border: none;
+  cursor: pointer;
+  background-color: transparent; 
+`;
+
+S.TextBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center; 
+  justify-content: center;
+  width: 464px;
+  padding-top: 50px;
+`
+S.Text = styled.p`
+  font-size: 14px;
+  color: #828282;
+  text-decoration: none;
+  background-color: transparent;
+`
+S.TextDivider = styled.p`
+  font-size: 14px;
+  color: #828282;
+  padding: 0px 5px;
+`
+S.JoinButton = styled.button`
+  font-size: 14px;
+  color: #828282;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+`
+S.Text2 = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  color: #828282;
+  text-align: center;
+  width: 464px;
+  padding-top: 20px;
+`
+
 
 export default S;
