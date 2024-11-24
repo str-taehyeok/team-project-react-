d2dd579ff026236487510ceac4788943448c638b
dev-minchul <minchul766@gmail.com>
2024-11-25 02:44:32 +0900

    Pet

--- src/pages/myhome/myPet/PetWrite.jsx
+++ src/pages/myhome/myPet/PetWrite.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import S from "./style";
+import { Link } from 'react-router-dom';
 
 const PetWrite = () => {
     return (
         <div>
-
+            <S.Mydog>
+                <S.PetCard>
+                    <S.Profilepic>이미지</S.Profilepic>
+                    <S.Name>이름</S.Name>
+                    <S.EditButton>이미지 편집</S.EditButton>
+                </S.PetCard>
+                    <div>
+                        <S.Title as="h5">
+                            <S.PetName>이름</S.PetName>
+                            <S.PetKind>반려종류</S.PetKind>
+                            <S.PetGender>성별</S.PetGender>
+                            <S.PetBreed>품종</S.PetBreed>
+                            <S.PetBrith>생일</S.PetBrith>
+                            <S.PetBreed>몸무게</S.PetBreed>
+                            </S.Title>
+                    </div>
+            </S.Mydog>
         </div>
     );
 };
--- src/pages/myhome/myPet/style.js
+++ src/pages/myhome/myPet/style.js
@@ -2,4 +2,74 @@ import styled from "styled-components";
 
 const S = {}
 
+S.Mydog = styled.div`
+  display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	margin: 0 auto;
+`
+S.PetCard = styled.div`
+  width: 270px;
+	height: 430px;
+	background-color: white;
+	border: 1px solid #ccc;
+	border-radius: 15px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`
+S.Profilepic = styled.div`
+  border-radius: 50%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 20px;
+`
+S.Name = styled.div`
+  font-weight: bold;
+	margin-bottom: 20px;
+`
+S.EditButton = styled.label`
+  background-color: #1c4da0;
+	color: white;
+	border: none;
+	border-radius: 20px;
+	cursor: pointer;
+	width: 96px;
+	height: 32px;
+	padding-top: 5px;
+	padding-left: 11px;
+`
+S.Title = styled.h5`
+	margin-left: 60px;
+	font-weight: bold;
+`
+S.PetName = styled.label`
+  display: block;
+  padding-top: 30px;
+`
+S.PetKind = styled.label`
+  display: block;
+  padding-top: 30px;
+`
+S.PetGender = styled.label`
+  display: block;
+  padding-top: 30px;
+`
+S.PetBreed = styled.label`
+  display: block;
+  padding-top: 30px;
+`
+S.PetBrith = styled.label`
+  display: block;
+  padding-top: 30px;
+`
+S.PetBreed = styled.label`
+  display: block;
+  padding-top: 30px;
+`
+
 export default S;
