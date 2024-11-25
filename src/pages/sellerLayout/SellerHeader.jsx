d51cc7a53f4a56fb4b463e7b35f29fe9e5d5ef19
dev-jane99 <ruejaeeun99@gmail.com>
2024-11-25 20:57:55 +0900

    jane sellerheader1

--- src/pages/sellerLayout/SellerHeader.jsx
+++ src/pages/sellerLayout/SellerHeader.jsx
@@ -1,55 +1,104 @@
-import React from 'react';
+import React, {useState} from 'react';
 import S from "./style";
+import { Link } from 'react-router-dom';
+
 
 const SellerHeader = () => {
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+const [activeSubmenu, setActiveSubmenu] = useState(null);
+const [currentMenu, setCurrentMenu] = useState("상품 관리");
+
+const menuItems = [
+    {
+        menu: "상품 관리",
+        submenu: [
+            { name: "상품조회", path: "/seller" },
+            { name: "상품 등록", path: "/seller/seller-write" }
+        ]
+    },
+    {
+        menu: "주문 관리",
+        submenu: [
+            { name: "주문조회", path: "/seller/seller-order-list" }
+        ]
+    },
+    {
+        menu: "정산 관리",
+        submenu: [
+            { name: "정산내역조회", path: "/seller/settlement" }
+        ]
+    },
+    {
+        menu: "판매자 관리",
+        submenu: [
+            { name: "판매자정보 관리", path: "/seller/info" },
+            { name: "사업자정보 관리", path: "/seller/business-info" }
+        ]
+    },
+    {
+        menu: "매출 관리",
+        submenu: [
+            { name: "매출 조회", path: "/seller/revenue-list" }
+        ]
+    }
+];
+
+const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+    setActiveSubmenu(null);
+};
+
+const handleMenuItemHover = (index) => {
+    if (isMenuOpen) {
+        setActiveSubmenu(index);
+    }
+};
+
+const handleMenuItemClick = (menu) => {
+    setCurrentMenu(menu);
+};
+
+const handleMenuLeave = () => {
+    setActiveSubmenu(null);
+};
+
+const handleFrameLeave = () => {
+    setIsMenuOpen(false);
+    setActiveSubmenu(null);
+};
+
+const handleLogout = () => {
+    // Handle logout logic here
+    window.location.href = '/seller-logout';
+};
+
     return (
         <S.Header>
-            판매자 헤더
             <S.MenuWrap>
-                <img className="menu-icon" src={process.env.PUBLIC_URL + "/assets/images/seller/menu.png"}
-                     alt="메뉴"/><span>매출관리</span>
-                {/*<div id="menu-frame">*/}
-                {/*        <div className="menu-header">*/}
-                {/*            <div className="welcome">seller님 환영합니다!</div>*/}
-                {/*            <button className="logout" onClick="location.href='seller-logout.seller'">로그아웃</button>*/}
-                {/*        </div>*/}
-                {/*        <div className="menu-form">*/}
-                {/*            <ul>*/}
-                {/*                <li>*/}
-                {/*                    <p>상품 관리</p>*/}
-                {/*                    <div>*/}
-                {/*                        <a href="../seller/seller-list.seller" style="color: white;"><p>상품조회</p></a>*/}
-                {/*                        <a href="../seller/seller-write.seller" style="color: white;"><p>상품 등록</p></a>*/}
-                {/*                    </div>*/}
-                {/*                </li>*/}
-                {/*                <li>*/}
-                {/*                    <p>주문 관리</p>*/}
-                {/*                    <div>*/}
-                {/*                        <a href="../seller/seller-order-list.seller" style="color: white;"><p>주문조회</p></a>*/}
-                {/*                    </div>*/}
-                {/*                </li>*/}
-                {/*                <li>*/}
-                {/*                    <p>정산 관리</p>*/}
-                {/*                    <div>*/}
-                {/*                        <a href="#" style="color: white;"><p>정산내역조회</p></a>*/}
-                {/*                    </div>*/}
-                {/*                </li>*/}
-                {/*                <li>*/}
-                {/*                    <p>판매자 관리</p>*/}
-                {/*                    <div>*/}
-                {/*                        <a href="#" style="color: white;"><p>판매자정보 관리</p></a>*/}
-                {/*                        <a href="#" style="color: white;"><p>사업자정보 관리</p></a>*/}
-                {/*                    </div>*/}
-                {/*                </li>*/}
-                {/*                <li>*/}
-                {/*                    <p>매출 관리</p>*/}
-                {/*                    <div>*/}
-                {/*                        <a href="../seller/seller-revenue-list.jsp" style="color: white;"><p>매출 조회</p></a>*/}
-                {/*                    </div>*/}
-                {/*                </li>*/}
-                {/*            </ul>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
+                <img src={process.env.PUBLIC_URL + "/assets/images/seller/menu.svg"} alt="메뉴"  onClick={handleMenuClick}/>
+                <span>{currentMenu}</span>
+                <S.MenuFrame style={{ display: isMenuOpen ? 'flex' : 'none' }} onMouseLeave={handleFrameLeave}>
+                        <S.MenuHeader>
+                            <S.Welcome>seller님 환영합니다!</S.Welcome>
+                            <button className="logout" onClick={handleLogout}>로그아웃</button>
+                        </S.MenuHeader>
+                        <S.MenuForm>
+                            <ul>
+                                {menuItems.map((item, index) => (
+                                    <li key={index} onMouseEnter={() => handleMenuItemHover(index)} onMouseLeave={handleMenuLeave}>
+                                        <p>{item.menu}</p>
+                                        <div>
+                                            {item.submenu.map((subItem, subIndex) => (
+                                                <Link key={subIndex} to={subItem.path}>
+                                                    <p>{subItem.name}</p>
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </S.MenuForm>
+                </S.MenuFrame>
             </S.MenuWrap>
         </S.Header>
     );
