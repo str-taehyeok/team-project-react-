c33dd14f0d4f2ad43e00a2a7fe911d4db8fe01b6
dev-jane99 <ruejaeeun99@gmail.com>
2024-11-28 00:26:33 +0900

    seller header finito

--- src/pages/sellerLayout/style.js
+++ src/pages/sellerLayout/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { flexCenterRowSpaceBetween, h7Medium, h6Bold } from '../../global/common'
 
 const S = {}
 
@@ -7,94 +8,110 @@ const S = {}
         flex-direction: column;
         width: 100dvw;
         height : 100dvh;
-        padding: 118px 0 0 0;
+        //padding: 118px 0 0 0;
     `;
-
     S.Header = styled.header`
-        width: 100dvw;
-        height : 118px;
-        border : solid 1px #000;
-        position: fixed;
-        z-index: 1000;
-        top: 0;
-        left: 0;
-        background-color: white;
-    `
+        border-bottom: solid 2px #828282;
+        background-color: #fff;
+        width : 1440px;
+        margin : 0 auto;
+        ${flexCenterRowSpaceBetween};
+    `;
 
     S.Main = styled.main`
         flex: 1;    
     `;
 
 
-    // header
-    
-    
+// header
+
+
     S.MenuWrap = styled.div`
-            display: flex;
-            align-items: center;
-            width: 1440px;
-            margin: 28px 0;
-            gap: 72px;
-            padding-left: 28px;
-            position: relative;
-            
-            & span{
-                font-weight: bold;
-                font-size: 20px;
-                margin-top: 4px;
-            }
-            
-            & img{
-                cursor: pointer;
-            }
-        `;
-    
-        S.MenuFrame = styled.div`
-            display: none;
-            flex-direction: column;
-            width: 1440px;
-            align-items: start;
-            animation: lift-detail 0.1s;
-            position: absolute;
-            top: 85px;
-            left: 35px;
-        `;
+        display: flex;
+        align-items: center;
+        width: 1440px;
+        margin: 27px 0;
+        gap: 72px;
+        padding-left: 28px;
+        position: relative;
         
-        S.MenuHeader = styled.div`
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 0;
-            margin: 0;
-            min-width: 200px;
-        `;
+        & span{
+            font-weight: bold;
+            font-size: 20px;
+            margin-top: 4px;
+            margin-left: 10px;
+        }
         
-        S.Welcome = styled.div`
-            font-size: 14px;
-            text-align: left;
-            vertical-align: bottom;
-        `;
+        & img{
+            cursor: pointer;
+            width: 50px;
+            height: 50px;
+            margin-top: 5px;
+        }
+    `;
+
+    S.MenuFrame = styled.div`
+        display: none;
+        flex-direction: column;
+        width: 1440px;
+        align-items: start;
+        animation: lift-detail 0.1s;
+        position: absolute;
+        top: 85px;
+        left: 0px;
+    `;
+
+    S.EntireMenu = styled.div`
+        background-color: #fff;
+    `;
+
+
+    S.MenuHeader = styled.div`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-bottom: 16px;
+        padding-top: 18px;
+        min-width: 241px;
+        background-color: #fff;
         
-        
-        // S.Logout = styled.button`
-        //     font-size: 10px;
-        //     color: #ffc303;
-        //     background-color: white;
-        //     border: none;
-        //     margin-left: auto;
-        //     font-weight: bold;
-        //     cursor: pointer;
-        //     margin-right: 24px;
-        //     vertical-align: bottom;
-        // `;
+        & button{
+            fontSize: 10px;
+            color: #ffc303;
+            background-color: white;
+            border: none;
+            marginLeft: auto;
+            fontWeight: bold;
+            cursor: pointer;
+            marginRight: 24px;
+        }
+    `;
+
+    S.Welcome = styled.div`
+        font-size: 16px;
+        text-align: left;
+        vertical-align: bottom;
+        background-color: #fff;
+    `;
+
+
+// S.Logout = styled.button`
+//     font-size: 10px;
+//     color: #ffc303;
+//     background-color: white;
+//     border: none;
+//     margin-left: auto;
+//     font-weight: bold;
+//     cursor: pointer;
+//     margin-right: 24px;
+//     vertical-align: bottom;
+// `;
 
     S.MenuForm = styled.div`
         & ul{
             list-style: none;
             padding: 0;
             width: 200px;
-            position: absolute;
-            z-index: 1000;
         }
         
         & li{
@@ -103,16 +120,25 @@ const S = {}
         }
         
         & p{
-            width: 200px;
-            height: 40px;
+            width: 243px;
+            height: 61px;
             background: #828282;
             color: white;
             display: flex;
             align-items: center;
             justify-content: flex-start;
             cursor: pointer;
-            padding-left: 15px;
-            margin: 16px 0;
+            padding-left: 24px;
+            margin: 5px 0;
+            ${h6Bold};
+        }
+        
+        & span{
+            display: flex;
+            align-items: center;
+            background-color: #fff;
+            color: #131313;
+            ${h7Medium};
         }
         
         & div{
@@ -121,16 +147,11 @@ const S = {}
             transition: height 0.3s ease;
             padding-left: 15px;
         }
+        
+        }
     `;
 
-    S.SubMenu = styled.div`
-        style
 
-    = {
-    {
-        height: activeSubmenu = = = index ? \` ${item.submenu.length * 40}px \`: '0'
-    }
-    }
-    `
+
 
 export default S;
