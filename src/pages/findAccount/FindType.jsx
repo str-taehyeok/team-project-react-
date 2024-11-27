1ee919b1806fb72b1a23cc17c268cf2b5f1f392c
dev-gina <jaleeck1113@gmail.com>
2024-11-27 23:33:21 +0900

    이진아 1127

--- src/pages/findAccount/FindType.jsx
+++ src/pages/findAccount/FindType.jsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import S from './style';
+import { FindContext } from '../../context/findContext';
+import { Link } from 'react-router-dom';
 
 const FindType = () => {
-    return (
-        <S.FindMain>
-            <S.LogoBox>
-                <img src={`${process.env.PUBLIC_URL}/assets/images/layout/powpow-logo.png`} alt="로고" />
-            </S.LogoBox>
-            <S.Text>찾고자 하는 계정을 선택해 주세요.</S.Text>
 
-            <S.RaidoBox>
-                <S.Label htmlFor="userA">
-                    <S.RadioInput name="user" id="userA" value="A" />
-                    <S.Text1>POWPOW 회원</S.Text1>
-                </S.Label>
-                <S.Label htmlFor="userB">
-                    <S.RadioInput name="user" id="userB" value="B" />
-                    <S.Text1>POWPOW 판매자</S.Text1>
-                </S.Label>
-            </S.RaidoBox>
-            <S.NextButton>다음</S.NextButton>
+    const { state, action } = useContext(FindContext)
+  
+    return (
+      <div>
+        <S.FindMain>
+          <Link to="/">
+            <S.LogoWrap>
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/images/layout/logo.png`}
+                alt="로고"
+              />
+            </S.LogoWrap>
+          </Link>
+  
+          <S.Text>찾고자하는 계정을 선택해주세요.</S.Text>
+  
+          <S.RadioBox>
+            <label htmlFor="UserA">
+              <input
+                type="radio"
+                name="user"
+                id="UserA"
+                value="buyer"
+                required
+                onChange={(e) => action.setMember(e.target.value)} // 값 설정
+              />
+              <p className="Text1">POWPOW 회원</p>
+            </label>
+            <label htmlFor="UserB">
+              <input
+                type="radio"
+                name="user"
+                id="UserB"
+                value="seller"
+                required
+                onChange={(e) => action.setMember(e.target.value)} // 값 설정
+              />
+              <p className="Text1">POWPOW 판매자</p>
+            </label>
+          </S.RadioBox>
+          <Link to={"//phone"}><S.NextButton>다음</S.NextButton></Link>
         </S.FindMain>
+      </div>
     );
-};
+  };
+  
 
 export default FindType;
--- src/pages/layout/Header.jsx
+++ src/pages/layout/Header.jsx
@@ -22,37 +22,37 @@ const Header = () => {
                 </S.LogoWrap>
                 <S.MenuWrap onMouseOver={handleMouseOver}>
                     <S.MainMenu>
-                        <li><Link to={""}>마이펫</Link></li>
-                        <li><Link to={""}>스토어</Link></li>
-                        <li><Link to={""}>커뮤니티</Link></li>
-                        <li><Link to={""}>마이홈</Link></li>
+                        <li><Link to={"/petsonal"}>펫스널컬러</Link></li>
+                        <li><Link to={"/store"}>스토어</Link></li>
+                        <li><Link to={"/community"}>커뮤니티</Link></li>
+                        <li><Link to={"/myhome"}>마이홈</Link></li>
                     </S.MainMenu>
                     <S.SubMenuWrap className={ isHover ? "active" : "" } >
                         <S.SubMenu>
-                            <li><Link to={""}>펫스널컬러테스트</Link></li>
+                            <li><Link to={"/petsonal"}>테스트</Link></li>
                             <li></li>
                         </S.SubMenu>
                         <S.SubMenu>
-                            <li><Link to={""}>강아지</Link></li>
-                            <li><Link to={""}>고양이</Link></li>
+                            <li><Link to={"/store-dog"}>강아지</Link></li>
+                            <li><Link to={"/store-cat"}>고양이</Link></li>
                         </S.SubMenu>
                         <S.SubMenu>
-                            <li><Link to={""}>추천 바구니</Link></li>
-                            <li><Link to={""}>내 게시물</Link></li>
+                            <li><Link to={"/post"}>추천 바구니</Link></li>
+                            <li><Link to={"/my-post-list"}>내 게시물</Link></li>
                         </S.SubMenu>
                         <S.SubMenu>
-                            <li><Link to={""}>나의 정보</Link></li>
-                            <li><Link to={""}>반려동물 정보</Link></li>
+                            <li><Link to={"profile-update"}>나의 정보</Link></li>
+                            <li><Link to={"notification"}>반려동물 정보</Link></li>
                         </S.SubMenu>
                     </S.SubMenuWrap>
                 </S.MenuWrap>
                 <S.IconWrap>
                     <Link to={""} ><img src={`${process.env.PUBLIC_URL}/assets/images/layout/search.png`} alt="로고"/></Link>
                     <Link to={""} ><img src={`${process.env.PUBLIC_URL}/assets/images/layout/delivery.png`} alt="로고"/></Link>
-                    <Link to={""} ><img src={`${process.env.PUBLIC_URL}/assets/images/layout/cart.png`} alt="로고"/></Link>
+                    <Link to={"/cart"} ><img src={`${process.env.PUBLIC_URL}/assets/images/layout/cart.png`} alt="로고"/></Link>
                     <p>|</p>
                     { member ? (
-                        <Link to={""}>로그아웃</Link>
+                        <Link to={"/logout"}>로그아웃</Link>
                     ) : (
                         <Link to={"/login"}>로그인</Link>
                     )}
