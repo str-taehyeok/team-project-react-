70e11e04c80d0c505b7fc68fd946a36c79e05e97
seyeon112 <lilylilyghost@gmail.com>
2024-12-02 09:08:58 +0900

    cart 추가

---
+++ src/context/heartContext.js
@@ -0,0 +1,22 @@
+import { act, createContext, useState } from "react";
+
+const HeartContext = createContext({
+  state : { heart : "" },
+  action : { setMember : () => {}}
+})
+
+const HeartProvider = ({children}) => {
+  const [heart, setHeart] = useState("noHeart");
+  const value = {
+    state : { heart : heart },
+    action : { setHeart, setHeart }
+  }
+
+  return (
+    <HeartContext.Provider value={value}>
+      {children}
+    </HeartContext.Provider>
+  )
+}
+
+export {HeartProvider, HeartContext};
--- src/pages/store/cart/CartProduct.jsx
+++ src/pages/store/cart/CartProduct.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import S from "./style";
+import { Link } from 'react-router-dom';
+
+const products = [
+  {
+    productImage : "cartProduct.png",
+    productName : "오쥬 바이 로우즈 독 치킨가슴살&호박 파우치 강아지 간식 69g (유통기한 2025-02225까지)",
+    productStock : 1,
+    productPrice : 4_500,
+    productDiscountPrice : 4_050,
+    productCoupon : 0,
+  }
+]
 
 const CartProduct = () => {
-  return (
-    <div>
+
+  const productList = products.map((product, i) => (
+    <S.CartProduct key={i}>
+      <img src={`${process.env.PUBLIC_URL}/assets/images/store/cartProduct.png`} alt="카트이미지"></img>
+      <S.CartProductInfo>
+        <p>{product.productName}</p>
+        <p>{product.productStock}</p>
       
-    </div>
+          <p>{product.productPrice} 원</p>
+          <p>{product.productDiscountPrice} 원</p>
+    
+        <p>{product.productCoupon}</p>
+      </S.CartProductInfo>
+    </S.CartProduct>
+      
+  ))
+
+  return (
+ 
+      <S.CartWrap>
+        {productList}
+      </S.CartWrap>
+      
+
   );
 };
 
