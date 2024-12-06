2a09c706150b9ffd8c9b946dd16dc004d0abde8b
dev-cdy <argon4067@gmail.com>
2024-12-06 10:33:31 +0900

    임시저장

--- src/pages/admin/coupon/CouponList.jsx
+++ src/pages/admin/coupon/CouponList.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
-
+import S from "./style";
 const CouponList = () => {
+
+    // const startdateInput = document.querySelector("#start-date");
+    // let startDate = `${adminBanner.adminBannerStart}`;
+    // startDate = startDate.split(" ")[0];
+    // startdateInput.value = startDate;
+    //
+    // const endDateInput = document.querySelector("#end-date");
+    // let endDate = `${adminBanner.adminBannerEnd}`;
+    // endDate = endDate.split(" ")[0];
+    // endDateInput.value = endDate;
+
+    const adminCoupon = [
+        {
+            adminCouponTitle : '쿠폰 제목',
+            adminCouponStart : '쿠폰시작날짜',
+            adminCouponEnd : '쿠폰마무리날짜',
+            adminCouponCode : '쿠폰코드',
+        },
+    ];
+
     return (
         <div>
-            쿠폰리스트
+            <S.CouponListTitle>
+                <S.h1>쿠폰 조회</S.h1>
+            </S.CouponListTitle>
+
+            <S.NoticeSearch>
+                <S.SearchBar>
+                    <S.Search type="text" placeholder="쿠폰명을 입력하세요"/>
+                </S.SearchBar>
+                <S.Buttons>
+                    {/*<button><Link to={"/myhome/profile-update"}>수정하기</Link></button>*/}
+                    <S.SearchBtn type="button">검색</S.SearchBtn>
+                    <S.ResetBtn type="button" onClick="location.reload();">초기화</S.ResetBtn>
+                    <S.NewAddBtn type="button" >신규 추가</S.NewAddBtn>
+                </S.Buttons>
+            </S.NoticeSearch>
+
+            <S.NoticeList>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>쿠폰명</th>
+                        <th>시작일</th>
+                        <th>종료일</th>
+                        <th>쿠폰코드</th>
+                        <th>관리</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    {/*<c:forEach var="adminCoupon" items="${adminCouponList}" varStatus="status">*/}
+                        <tr>
+                            {/*<td>${status.index + 1}</td>*/}
+                            <td>${adminCoupon.adminCouponTitle}</td>
+                            <td>${adminCoupon.adminCouponStart}</td>
+                            <td>${adminCoupon.adminCouponEnd}</td>
+                            <td>${adminCoupon.adminCouponCode}</td>
+                            <td>
+                                <div className="change-buttons">
+                                    {/*<button type="button" className="edit-btn"*/}
+                                    {/*        onClick="location.href='admin-coupon-update.admin?id=${adminCoupon.id}'">수정*/}
+                                    {/*</button>*/}
+                                    <button type="button" className="delete-btn"
+                                            data-id="${adminCoupon.id}">삭제
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    {/*</c:forEach>*/}
+                    </tbody>
+                </table>
+            </S.NoticeList>
         </div>
     );
 };
--- src/pages/admin/coupon/style.js
+++ src/pages/admin/coupon/style.js
@@ -2,4 +2,233 @@ import styled from "styled-components";
 
 const S = {}
 
+/*****상세내용*******/
+
+S.CouponListTitle = styled.div`
+    margin: 42px 0 35px 0;
+    display: flex;
+    justify-content: space-between;
+    width: 1191px;
+`
+
+S.h1 = styled.p`
+    font-size: 40px;
+    font-weight: bold;
+`
+
+// .new-add-btn {
+//     width: 120px;
+//     height: 30px;
+//     border: none;
+//     border-radius: 10px;
+//     color: white;
+//     cursor: pointer;
+//     background-color: #113F8A;
+//     left: 1082px;
+//     top: 134px;
+// }
+//
+//
+S.NoticeSearch = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 1440px;
+`
+
+S.SearchBar = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 10px;
+`
+
+S.Search = styled.input`
+    width: 708px;
+    height: 60px;
+    border-radius: 20px;
+    padding: 17px;
+`
+
+S.Buttons = styled.div`
+    display: flex;
+    margin: 35px 0 180px 0;
+    gap: 10px;
+`
+
+S.SearchBtn = styled.button`
+    width: 90px;
+    height: 30px;
+    border: solid 1px #888888;
+    border-radius: 20px;
+    color: #131313;
+    cursor: pointer;
+    background-color: #fff;
+`
+S.ResetBtn = styled.button`
+    width: 90px;
+    height: 30px;
+    border: solid 1px #888888;
+    border-radius: 20px;
+    color: #131313;
+    cursor: pointer;
+    background-color: #fff;
+`
+
+S.NewAddBtn = styled.button`
+    width: 90px;
+    height: 30px;
+    border: solid 1px #113F8A;
+    border-radius: 20px;
+    color: #fff;
+    cursor: pointer;
+    background-color: #113F8A;
+`
+
+S.NoticeList = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 1440px;
+    margin: 0 auto;
+`;
+
+//
+// label {
+//     display: block;
+//     text-align: center;
+//     font-weight: bold;
+// }
+//
+// table {
+//     width: 100%;
+//     text-align: center;
+//     table-layout: fixed;
+// }
+//
+// table tr {
+//     display: flex;
+// }
+//
+// table tr > th {
+//     width: 100%;
+//     border-bottom: solid 0.1px #828282;
+//     justify-content: center;
+//     padding-bottom: 28px;
+// }
+//
+// table tr > td {
+//     width: 100%;
+//     justify-content: center;
+//     padding: 20px 0;
+//     gap: 5px;
+// }
+//
+// table tr > th:nth-child(1){
+//     width: 122px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+// table tr > th:nth-child(2){
+//     width: 366px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+// table tr > th:nth-child(3){
+//     width: 244px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+// table tr > th:nth-child(4){
+//     width: 244px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+// table tr > th:nth-child(5){
+//     width: 366px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+// table tr > th:nth-child(6){
+//     width: 98px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+//
+// table tr > td:nth-child(1){
+//     width: 122px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+//
+// table tr > td:nth-child(2){
+//     width: 366px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+//
+// table tr > td:nth-child(3){
+//     width: 244px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+// table tr > td:nth-child(4){
+//     display: flex;
+//     width: 244px;
+//     flex-shrink: 0;
+//     align-items: center;
+// }
+// table tr > td:nth-child(5){
+//     width: 366px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+// table tr > td:nth-child(6){
+//     width: 98px;
+//     flex-shrink: 0;
+//     display: flex;
+//     align-items: center;
+// }
+//
+// .change-buttons {
+//     display: flex;
+//     gap: 10px;
+//
+// }
+//
+// .edit-btn {
+//     background-color: #888888;
+//     color: white;
+//     border: none;
+//     border-radius: 10px;
+//     cursor: pointer;
+//     width: 45px;
+//     height: 25px;
+// }
+//
+// .delete-btn {
+//     color: #131313;
+//     border: solid 1px #888888;
+//     background-color: #fff;
+//     border-radius: 10px;
+//     cursor: pointer;
+//     width: 45px;
+//     height: 25px;
+// }
+
+
+
+
+
 export default S;
