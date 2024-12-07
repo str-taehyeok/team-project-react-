c2f6897c58af133f8f68c184bce0125321f26067
soyeon-00 <soyeonlee0515@gmail.com>
2024-12-08 04:57:17 +0900

    게시물 수정 완료 페이지 및 알럿 작업

--- src/pages/admin/notice/NoticeUpdate.jsx
+++ src/pages/admin/notice/NoticeUpdate.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import S from "./style";
+import { Link } from 'react-router-dom';
+
+const datas = [
+  {
+    noticeTile : "공지사항 제목 작성 TEST",
+    noticeContent : "공지사항 내용 작성 TEST 하하하하하ㅏ하ㅏ하하핳ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ핳하하하ㅏ하ㅏ하하ㅏ하하ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ하ㅏ하ㅏ하하하ㅏ"
+  }
+]
+
 
 const NoticeUpdate = () => {
+  const onDelete = () => {
+    if (window.confirm("정말 취소하시나요??")) {
+      window.location.href = '/admin';
+    } else {
+      window.location.href = '/admin/update';
+    }
+};
+
+  const updates = datas.map(({noticeTile,noticeContent }, i) => (
+    <S.WriteBox key={i}>
+      <S.Title>
+        공지사항 수정
+      </S.Title>
+      <S.TextInputBox>
+        <S.TitleText>제목</S.TitleText>
+        <S.TextInput><input placeholder='제목을 입력해주세요' value={noticeTile}></input></S.TextInput>
+      </S.TextInputBox>
+      <S.InputBox>
+        <S.ContentText>내용</S.ContentText>
+        <S.ContentInput><textarea placeholder='내용을 입력해주세요' value={noticeContent}></textarea></S.ContentInput>
+      </S.InputBox>
+      <S.ButtonBox>
+        <Link to={`/admin`}>
+          <button className='done'>완료</button>
+        </Link>
+          <button className='cancel' onClick={onDelete}>취소</button>
+      </S.ButtonBox>
+    </S.WriteBox>
+));
+
   return (
-    <div>
-      
-    </div>
+    <>
+    {updates}
+    </>
   );
 };
 
