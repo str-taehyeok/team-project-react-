3aed99cacf750b4614e712123a367aae92ec7188
soyeon-00 <soyeonlee0515@gmail.com>
2024-12-07 18:01:23 +0900

    진짜 커뮤 조오금 남음..git add .

--- src/pages/community/post/UserPost.jsx
+++ src/pages/community/post/UserPost.jsx
@@ -4,11 +4,13 @@ import Slide from './Slide.jsx';
 import { Link, useSearchParams } from 'react-router-dom';
 import FollowBtn from './FollowBtn.jsx';
 import HeartBlackBtn from './HeartBlacktBtn.jsx';
+import HeartBtn from './HeartBtn.jsx';
 
 const myPost = [
     { 
         memberNickname : "쏘오연",
         memberImage : "/assets/images/community/default-myProfile.png ",
+        memberImage1 : "/assets/images/community/default-profile.png ",
         postTitle : "반려동물 게시물",
         postContent :   "게시물에 적은 본문 내용이 여기에 적힙니다." + 
                         "# 태그를 이용해서 연관 검색어에 걸리도록 작성가능합니다." +
@@ -16,10 +18,28 @@ const myPost = [
                         "당신의 반려동물에 관한건 뭐든지요." +
                         "사진은 필참이기에 게시글만 올라올 가능성은 없습니다.",
         likeCount : "0",
-        commentCount : "0"
+        commentCount : "0",
+        commentContent : "댓글 TEST 작업주웅"
     }
 ]
 
+const datas = [
+    {
+        postImage1 : "/assets/images/community/myPost1.png",
+    },
+    {
+        postImage1 : "/assets/images/community/myPost2.png",
+    },
+    {
+        postImage1 : "/assets/images/community/myPost3.png",
+    },
+    {
+        postImage1 : "/assets/images/community/myPost4.png",
+    },
+    {
+        postImage1 : "/assets/images/community/myPost5.png",
+    },
+]
 
 const UserPost = () => {
 
@@ -27,8 +47,9 @@ const UserPost = () => {
     const id = serachParams.get("postId")
     console.log(id)
 
-    const myPostPage = myPost.map(({memberImage, memberNickname, postTitle, postContent, likeCount, commentCount}, i) => (
+    const myPostPage = myPost.map(({memberImage, memberImage1, memberNickname, postTitle, postContent, likeCount, commentCount, commentContent}, i) => (
         <S.MyPostPage key={i}>
+            <S.MyPostPageBox>
             <S.MyPostBox>
                 <S.ProfileBox>
                     <S.MyProfile>
@@ -51,19 +72,44 @@ const UserPost = () => {
                 </S.MyPostUnderBox>
             </S.MyPostBox>
 
-            <S.CommentNumber>
-                <S.Comment>댓글</S.Comment>
-                <S.Comment>{commentCount}</S.Comment>
-            </S.CommentNumber>
-            <S.InputContainer>
-                <S.MyProfile>
-                    <img src={memberImage} alt="프로필" />
-                </S.MyProfile>
-                <input placeholder='댓글을 입력해주세요.'></input>
-                <button>완료</button>
-            </S.InputContainer>
+            <S.CommentDiv>
+                <S.CommentNumber>
+                    <S.Comment>댓글</S.Comment>
+                    <S.Comment>{commentCount}</S.Comment>
+                </S.CommentNumber>
+                <S.MyComment>
+                    <S.MyProfile>
+                        <img src={memberImage} alt="프로필" />
+                    </S.MyProfile>
+                    <S.InputContainer>
+                        <input placeholder='댓글을 입력해주세요.'></input>
+                        <button>완료</button>
+                    </S.InputContainer>
+                </S.MyComment>
+                <S.CommentBox>
+                    <Link to={`/post/list?postId=${id}`}>
+                        <img src={memberImage1} alt="프로필" />
+                    </Link>
+                    <S.CommentText>
+                        <S.NickName>{memberNickname}</S.NickName>
+                        <p>{commentContent}</p>
+                    </S.CommentText>
+                    <S.DotBtn><button><img src="/assets/images/community/dots.png" alt="삼점메뉴" /></button></S.DotBtn>
+                </S.CommentBox>
+            </S.CommentDiv>
+            </S.MyPostPageBox>
+            <S.BestText>유저들의 비슷한 공감 베스트</S.BestText>
         </S.MyPostPage>
     ))  
+
+    const postList = datas.map((post, i) => (
+        <S.BestPostBox key={i}>
+            <HeartBtn />
+            <Link to={`/post/read?postId=${id}`}>
+                <S.PostImage src={`${process.env.PUBLIC_URL}${post.postImage1}`} alt="게시물10" />
+            </Link>
+        </S.BestPostBox>
+    ))
 
     return (
         
@@ -71,51 +117,9 @@ const UserPost = () => {
             
             
                 {myPostPage}
-            {/* <S.Post>
-                    <div className='my-post-box'>
-                        <div className='my-post'>
-                            <div className='my-profile'>
-                                <img className='profile' src="/assets/images/community/profile.svg" alt="나의 프로필" />
-                                <p className='nick-name'>soyeon__0.0</p>
-                            </div>
-                                <FollowBtn />
-                            
-                            <Slide />
-                            <div className='box'>
-                                <div className='my-like'>
-                                    <img className='like' src="/assets/images/community/like.svg" alt="하트" />
-                                    <p className='like-number'>0</p>
-                                    <img className='dot' src="/assets/images/community/dots.svg" alt="삼점메뉴" />
-                                </div>
-                                <div className='text1'>
-                                    <p>게시물에 적은 본문 내용이 여기에 적힙니다<br/>
-                                    # 태그를 이용해서 연관 검색어에 걸리도록 작성가능합니다.<br/>
-                                    아무거나 적어주세요.<br/>
-                                    당신의 반려동물에 관한건 뭐든지요.<br/>
-                                    사진은 필참이기에 게시글만 올라올 가능성은 없습니다.<br/>
-                                    아름다운 말만 적어주세요.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='my-comment'>
-                            <div className='comment-number'>
-                                <p className='text2'>댓글</p>
-                                <p className='text2'>0</p>
-                            </div>
-                            <div className='box1'>
-                                <img className='profile2' src="/assets/images/community/profile.svg" alt="나의 프로필" />
-                                <div className='input-container'>
-                                    <input className='input' placeholder='댓글을 입력해주세요.'></input>
-                                    <button className='button'>완료</button>
-                                </div>
-                            </div>
-                            <div className='comment-box'>
-                                <p className='text3'>등록된 댓글이 없습니다.</p>
-                            </div>
-                        </div>
-                    </div>
-
-            </S.Post> */}
+                <S.AllPostWrapper>
+                    {postList}
+                </S.AllPostWrapper>
             {/* <S.BestPost>
                 <div className='bestBox'>
                     <p className='text4'>유저들의 비슷한 공감 베스트</p>
--- src/pages/community/post/style.js
+++ src/pages/community/post/style.js
@@ -178,169 +178,7 @@ S.PropileBox = styled.div`
   align-items:center;
   margin-top: 5px;
 `
- 
 
-
-
-
-// 컬러별 게시물
-// S.Post = styled.div`
-
-//     input:focus {
-//       outline: none;
-//     }
-
-//   & .mySwiper {
-//       width: 586px;
-//       height:586px;
-//       border-radius:20px;
-//    }
-   
-//   .like-number {
-//     font-size: 14px;
-//     line-height: 22px;
-//     letter-spacing: -3%;
-//     font-weight: bold;
-//     width: 100%;
-//   }
-
-//   .my-like{
-//     display:flex;
-//     flex-direction: row;
-//     align-items:center;
-//   }
-
-
-//   .text1{
-//     margin-top: 25px;
-//     font-size: 14px;
-//     line-height: 22px;
-//     letter-spacing: -3%;
-//   }
-//   .box{
-//     width:586px;
-//     padding: 19px 26px;
-//   }
-
-//   .my-post{
-//     display:flex;
-//     flex-direction: column;
-//     justify-content:center;
-//     align-items:center;
-//     width:586px;
-//   }
-
-//   .text2 {
-    // font-size: 20px;
-    // line-height: 30px;
-    // letter-spacing: -3%;
-    // font-weight: bold;
-//   }
-//   .comment-number{
-    // display:flex;
-    // flex-direction: row;
-    // align-items:center;
-    // margin-bottom: 5px;
-    // width:559px;
-    // justify-content:left;
-    // gap:10px;
-//   }
-//   .profile2{
-//     width:44px;
-//     height:44px
-//   }
-//   .input{
-    // border-radius: 20px;
-    // border-style: none;
-    // border: solid 1px #828282;
-    // width:469px;
-    // height:56px;
-    // padding: 19px 26px;
-    // font-size: 12px;
-    // line-height: 18px;
-    // letter-spacing: -3%;
-    // color:#828282;
-//   }
-//   .button{
-//     border:none;
-//     color: #113F8A;
-//     font-size: 12px;
-//     line-height: 18px;
-//     letter-spacing: -3%;
-//     background-color: white;
-//     position: absolute; 
-//     right: 20px; 
-//     top: 18px;
-//   }
-
-//   .input-container{
-//     position: relative; 
-//     width: 469px;
-//   }
-
-//   .box1 {
-//     display:flex;
-//     flex-direction: row;
-//     align-items: center;
-//     gap:28px;
-//     margin-top: 53px;
-//   }
-//   .comment-box{
-//     width: 559px;
-//     height: 681px;
-//     margin-top: 5px;
-//   }
-//   .text3{
-//     font-size: 14px;
-//     line-height: 22px;
-//     text-align: center;
-//    padding-top: 66px;
-//   }
-
-//   .my-comment{
-//     display:flex;
-//     flex-direction: column;
-//     width: 559px;
-//   }
-
-//   .my-post-box{
-//     display:flex;
-//     flex-direction: row;
-//     width:1178px;
-//     gap: 50px;
-//     margin: 0 auto;
-//     margin-top: 152px;
-//   }
-// `
-
-// // 베스트 게시물
-// S.BestPost = styled.div`
-
-//   .text4{
-//     font-size: 20px;
-//     line-height: 30px;
-//     font-weight:bold;
-//   }
-//   .bestPost{
-//     width: 170px;
-//     height: 170px;
-//     border-radius: 20px;
-//   }
-//   .best-post-box{
-//     display: flex;
-//     flex-direction: row;
-//     gap: 20px;
-//     margin-top: 39px;
-//   }
-//   .bestBox{
-//     display:flex;
-//     flex-direction: column;
-//     width:1132px;
-//     margin: 0 auto;
-//     margin-top: 10px;
-//     margin-bottom: 221px;
-//   }
-// `
   S.NickName = styled.div`
     font-size: 14px;
     line-height: 22px;
@@ -484,18 +322,21 @@ S.CommentNumber = styled.div`
 `
 
 S.InputContainer = styled.div`
+  position: relative; 
+  width: 469px;
   & input {
     border-radius: 20px;
     border-style: none;
     border: solid 1px #828282;
     width:469px;
     height:56px;
-    padding: 19px 26px;
+    padding: 19px 59px 19px 26px;
     font-size: 12px;
     line-height: 18px;
     letter-spacing: -3%;
     color:#828282;
     outline:none;
+    margin-left: 10px;
   }
   & button {
     border: none;
@@ -504,15 +345,53 @@ S.InputContainer = styled.div`
     color: #113F8A;
     font-size: 12px;
     font-weight: bold;
-    /* position: absolute;  */
-    /* right: 20px; 
-    top: 18px; */
-  }
-  & img{
     position: absolute; 
-    right: 20px; 
-    top: 18px;
+    right: 15px; 
+    top: 19px;
   }
+
+`
+S.MyComment = styled.div`
+  display:flex;
+  flex-direction: row;
+  align-items:center;
+`
+S.CommentBox = styled.div`
+  display:flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 32px;
+`
+S.CommentText = styled.div`
+  display:flex;
+  flex-direction: column;
+  margin-left: 10px;
+  width: 100%;
+`
+S.CommentDiv = styled.div`
+  width: 561px;
+
+`
+S.MyPostPageBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 52px;
+`
+S.BestPostBox = styled.div`
+position: relative;
+  .like-icon{
+    position: absolute;
+    right: 20px;
+    top : 20px;
+    width: 20px;
+    height:20px;
+  }
+`
+S.BestText = styled.div`
+  margin-top: 18px;
+  font-size: 24px;
+  line-height: 36px;
+  font-weight:bold;
 `
 
 
