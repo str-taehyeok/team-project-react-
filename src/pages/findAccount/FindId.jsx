78901624df1e6ffff659f4d87fca411c821893de
dev-gina <jaleeck1113@gmail.com>
2024-12-08 18:30:06 +0900

    1208 이진아

--- src/pages/findAccount/FindId.jsx
+++ src/pages/findAccount/FindId.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { FindContext } from "../../context/findContext";
 import S from "./style";
 
 const FindId = () => {
