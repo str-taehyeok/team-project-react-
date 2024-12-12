86dd5356adce8528f578d86d2b5926567a05573c
dev-tpghks23 <tpghksgit23@gmail.com>
2024-12-12 14:35:26 +0900

    like 추가

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import theme from "./global/theme";
 import GlobalStyle from "./global/global";
 import {RouterProvider} from "react-router-dom";
 import router from "./routes/router";
+import { HeartProvider } from './context/heartContext';
 
 function App() {
   return (
       <>
           <ThemeProvider theme={theme}>
-              <GlobalStyle />
-              <RouterProvider router={router} future={{ v7_startTransition: true }} />
+                <HeartProvider>
+                    <GlobalStyle />
+                    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+                </HeartProvider>
           </ThemeProvider>
       </>
   );
--- src/context/heartContext.js
+++ src/context/heartContext.js
@@ -1,15 +1,58 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const HeartContext = createContext({
-  state : { heart : "" },
-  action : { setMember : () => {}}
+  state : { 
+    postLikes : [], 
+    commLikes : [],
+    isUpdate : false,
+  },
+  action : { 
+    setPostLikes : () => {},
+    setCommLikes : () => {},
+    setIsUpdate : () => {}
+  }
 })
 
 const HeartProvider = ({children}) => {
-  const [heart, setHeart] = useState("noHeart");
+
+  // 리덕스에 있는 memberId
+  const memberId = 1;
+  const [ postLikes, setPostLikes ] = useState([]);
+  const [ commLikes, setCommLikes ] = useState([]);
+  const [ isUpdate, setIsUpdate ] = useState(false);
+
+  useEffect(() => {
+
+    // 커뮤니티 좋아요 전체 조회
+    const getCommLikes = async () => {
+      const response = await fetch(`http://localhost:10000/commLikes/allLikes`, {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(memberId)
+      });
+      const data = await response.json();
+      return data;
+    };
+
+    getCommLikes().then(setCommLikes).catch(console.error);
+
+    // 포스트 1개 좋아요 전체 조회
+
+  }, [isUpdate]);
+
   const value = {
-    state : { heart : heart },
-    action : { setHeart : setHeart }
+    state : { 
+      postLikes : postLikes, 
+      commLikes : commLikes,
+      isUpdate : isUpdate
+    },
+    action : { 
+      setPostLikes : setPostLikes,
+      setCommLikes : setCommLikes,
+      setIsUpdate : setIsUpdate
+    }
   }
 
   return (
