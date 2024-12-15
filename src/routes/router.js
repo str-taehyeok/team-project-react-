5552e880608ae61b687ce02a774249b439a77f91
dev-cdy <argon4067@gmail.com>
2024-12-16 02:46:31 +0900

    수정왜안됨

--- src/routes/router.js
+++ src/routes/router.js
@@ -427,7 +427,7 @@ const router = createBrowserRouter([
                         element: <CouponWrite />
                     },
                     {
-                        path: `coupon-update/:id`,
+                        path: "coupon-update/:id",
                         element: <CouponUpdate />
                     },
                 ]
