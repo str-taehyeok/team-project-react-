39d01ab39e9713757ab095ad6ea33decb9b2fa59
soyeon-00 <soyeonlee0515@gmail.com>
2024-12-18 18:33:58 +0900

    풀 받기전 셀러 정보 임시 저장

--- src/pages/sellerLayout/SellerHeader.jsx
+++ src/pages/sellerLayout/SellerHeader.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import S from "./style";
-import { useDispatch } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import { setUser, setUserStatus } from "../../modules/user";
 
 const SellerHeader = () => {
@@ -9,6 +9,13 @@ const SellerHeader = () => {
   const [activeSubmenu, setActiveSubmenu] = useState(null);
   const [currentTitle, setCurrentTitle] = useState("상품 관리");
   const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+
+  const id = currentUser.id;
+
+  if(!id){
+    return ;
+  }
 
   const handleLogout = () => {
     localStorage.removeItem("jwtToken");
@@ -34,9 +41,9 @@ const SellerHeader = () => {
       submenu: [{ name: "정산내역조회", path: "/seller/seller-adjustment" }],
     },
     {
-      title: [{ name: "판매자 관리", path: "/seller/seller-info" }],
+      title: [{ name: "판매자 관리", path: `/seller/seller-info` }],
       submenu: [
-        { name: "판매자정보 관리", path: "/seller/seller-info" },
+        { name: "판매자정보 관리", path: `/seller/seller-info/${id}` },
         {
           name: "사업자정보 관리",
           path: "/seller/seller-info/business-update",
@@ -76,68 +83,68 @@ const SellerHeader = () => {
   };
 
   return (
-    <S.Header>
-      <S.MenuWrap>
-        <img
-          src={process.env.PUBLIC_URL + "/assets/images/seller/menu.svg"}
-          alt="메뉴"
-          onClick={handleMenuClick}
-        />
-        <span>{currentTitle}</span>
-        <S.EntireMenu>
-          <S.MenuFrame
-            style={{ display: isMenuOpen ? "flex" : "none" }}
-            onMouseLeave={handleFrameLeave}
-          >
-            <S.MenuHeader>
-              <S.Welcome>seller님 환영합니다!</S.Welcome>
-              <Link to="/login" onClick={handleLogout}>
-                로그아웃
-              </Link>
-            </S.MenuHeader>
-            <S.MenuForm>
-              <ul>
-                {menuItems.map((item, index) => (
-                  <li
-                    key={item.title[0].name}
-                    onMouseEnter={() => handleMenuItemHover(index)}
-                    onMouseLeave={handleMenuLeave}
-                  >
-                    <Link
-                      to={item.title[0].path}
-                      onClick={() => handleMenuItemClick(item.title[0].name)}
-                    >
-                      <p>{item.title[0].name}</p>
-                    </Link>
-                    <div
-                      style={{
-                        height:
-                          activeSubmenu === index
-                            ? `${item.submenu.length * 30}px`
-                            : "0",
-                      }}
-                    >
-                      {item.submenu.map((subItem, subIndex) => (
+      <S.Header>
+        <S.MenuWrap>
+          <img
+              src={process.env.PUBLIC_URL + "/assets/images/seller/menu.svg"}
+              alt="메뉴"
+              onClick={handleMenuClick}
+          />
+          <span>{currentTitle}</span>
+          <S.EntireMenu>
+            <S.MenuFrame
+                style={{ display: isMenuOpen ? "flex" : "none" }}
+                onMouseLeave={handleFrameLeave}
+            >
+              <S.MenuHeader>
+                <S.Welcome>seller님 환영합니다!</S.Welcome>
+                <Link to="/login" onClick={handleLogout}>
+                  로그아웃
+                </Link>
+              </S.MenuHeader>
+              <S.MenuForm>
+                <ul>
+                  {menuItems.map((item, index) => (
+                      <li
+                          key={item.title[0].name}
+                          onMouseEnter={() => handleMenuItemHover(index)}
+                          onMouseLeave={handleMenuLeave}
+                      >
                         <Link
-                          key={`${item.title[0].name}-submenu-${subIndex}`}
-                          to={subItem.path}
-                          style={{ color: "white" }}
-                          onClick={() =>
-                            handleMenuItemClick(item.title[0].name)
-                          }
+                            to={item.title[0].path}
+                            onClick={() => handleMenuItemClick(item.title[0].name)}
                         >
-                          <span>{subItem.name}</span>
+                          <p>{item.title[0].name}</p>
                         </Link>
-                      ))}
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </S.MenuForm>
-          </S.MenuFrame>
-        </S.EntireMenu>
-      </S.MenuWrap>
-    </S.Header>
+                        <div
+                            style={{
+                              height:
+                                  activeSubmenu === index
+                                      ? `${item.submenu.length * 30}px`
+                                      : "0",
+                            }}
+                        >
+                          {item.submenu.map((subItem, subIndex) => (
+                              <Link
+                                  key={`${item.title[0].name}-submenu-${subIndex}`}
+                                  to={subItem.path}
+                                  style={{ color: "white" }}
+                                  onClick={() =>
+                                      handleMenuItemClick(item.title[0].name)
+                                  }
+                              >
+                                <span>{subItem.name}</span>
+                              </Link>
+                          ))}
+                        </div>
+                      </li>
+                  ))}
+                </ul>
+              </S.MenuForm>
+            </S.MenuFrame>
+          </S.EntireMenu>
+        </S.MenuWrap>
+      </S.Header>
   );
 };
 
