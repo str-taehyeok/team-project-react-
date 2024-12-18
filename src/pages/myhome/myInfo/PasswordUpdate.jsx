611607c37af7e8d1dbd1f07bfee8563f665f25de
dev-cdy <argon4067@gmail.com>
2024-12-18 15:54:35 +0900

    passwordupdate

--- src/pages/myhome/myInfo/PasswordUpdate.jsx
+++ src/pages/myhome/myInfo/PasswordUpdate.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import S from "./style";
+import { useSelector } from "react-redux";
 
 
 const PasswordUpdate = () => {
+
+        const { currentUser } = useSelector((state) => state.user);
+        const [currentPassword, setCurrentPassword] = useState('');
+        const [newPassword, setNewPassword] = useState('');
+        const [confirmNewPassword, setConfirmNewPassword] = useState('');
+        const [message, setMessage] = useState('');
+
+
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+
+            if (newPassword !== confirmNewPassword) {
+                setMessage('새로운 비밀번호가 일치하지 않습니다.');
+                return;
+            }
+
+            if (!passwordRegex.test(newPassword)) {
+                setMessage('비밀번호는 소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다.');
+                return;
+            }
+
+            try {
+                const response = await fetch('http://localhost:10000/member/check-password', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        memberEmail: currentUser.email,
+                        memberPassword: currentPassword,
+                    }),
+                });
+
+                const result = await response.json();
+
+                if (result.success) {
+                    setMessage('비밀번호가 성공적으로 업데이트되었습니다.');
+                } else {
+                    setMessage('현재 비밀번호가 일치하지 않습니다.');
+                }
+            } catch (error) {
+                setMessage('비밀번호 변경 중 오류가 발생했습니다.');
+            }
+        };
+
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
-    const usedPassword ="123456a@";
     return (
         <S.PasswordUpdateFormContainer>
             <S.PasswordUpdateForm>
