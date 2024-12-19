dff54f1059345fdf46f64d07232969d5a5789b87
seyeon112 <lilylilyghost@gmail.com>
2024-12-19 09:10:19 +0900

    세연 리액트 추가

--- src/pages/myhome/myInfo/likestyle.js
+++ src/pages/myhome/myInfo/likestyle.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import {
-    h1Bold, h7Bold, h8Regular, gray500, h7Regular, h2Bold, h6Regular, h9Regular, h6Bold
+    h1Bold, h7Bold, h8Regular, gray500, h7Regular, h2Bold, h6Regular, h9Regular, h6Bold, h9Bold
 } from '../../../global/common'
 import theme from "../../../global/theme";
 import { Link } from 'react-router-dom'
@@ -36,14 +36,23 @@ S.LikeListNoneBox = styled.div`
 
 
 S.LikesAll = styled.div`
-    width: 1440px;
+    width: 822px;
     margin: 0 auto;
     
+`
+
+S.LikeStoreWrap = styled.div`
+    width: 822px;
+    display: block;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
 `
 
 // 스토어 좋아요 영역 제목
 S.LikeStoreTItleWrap = styled.div`
     margin-top: 102px;
+    margin-bottom: 18px;
     width: 822px;
     height: 30px;
     display: flex;
@@ -64,7 +73,103 @@ S.OtherClickBtn = styled.div`
     width: 112px;
     height: 18px;
     ${h9Regular}
+    cursor : pointer;
+`
+S.ProductLikes = styled.div`
+    width: 190px;
+    height: 259px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    & > img {
+        height: 190px;
+        border-radius: 20px;
+        width: 100%;
+        cursor: pointer;
+    }
+`
+
+S.ProductName = styled.div`
+    width: 127px;
+    height: 36px;
+    display: flex;
+    flex-wrap: wrap;
+
+    
+    & > p {
+        ${h9Bold}
+    }
+    
+`
+
+S.ProductPrice = styled.div`
+    & > p {
+        ${h9Regular}
+    }
+
+`
+// 제품 전체 래퍼
+S.productLikeList = styled.div`
+    width: 823px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: first;
+    gap: 21px;
+    margin-bottom: 86px;
+    
+`
+
+S.ProfileInfo = styled.div`
+    
+    & > img {
+        width: 40px;
+        height: 40px;
+    }
+    
+    & span {
+        margin-left: 10px;
+        ${h9Bold}
+    }
+
+`
+
+S.LikeCommTitleWrap = styled.div`
+    margin-top: 102px;
+    margin-bottom: 18px;
+    width: 822px;
+    height: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    ${h6Bold}
+
+`
+
+S.CommTitle = styled.div`
+    width: 72px;
+    height: 30px;
+    ${h6Bold}
+
+`
+
+S.OtherClickCommBtn = styled.div`
+    width: 123px;
+    height: 18px;
+    ${h9Regular}
     cursor: pointer;
 `
+
+S.CommLikeList = styled.div`
+    width: 823px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: first;
+    gap: 21px;
+    margin-bottom: 155px;
+
+`
+
 
 export default S;
